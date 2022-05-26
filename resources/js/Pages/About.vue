f714a95cb22c726212ae11774a8c09289c4a84bc
Maclevison Silva <maclevison@gmail.com>
2022-05-26 15:47:50 +0100

    chore: initial boilerplate code

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-zinc-100 h-screen w-screen flex flex-col items-center justify-center">
+        <h1 class="text-9xl font-bold mt-5 w-full text-center">About Page</h1>
+        <a href="/"> Back to home</a>
+    </div>
+</template>
+
+<script setup></script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-zinc-100 h-screen w-screen flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold mt-5 w-full text-center">Hello world</h1>
+        <div class="text-center text-xl">Inertiajs + Vue + Tailwindcss + Laravel 😁</div>
+        <a href="/about">Go to about page ></a>
+    </div>
+</template>
+
+<script setup></script>
